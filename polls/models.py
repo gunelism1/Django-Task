9a06ff85b2ac766d47d1bada858d9159d061168e
gunelism1 <gunelism1@example.com>
2025-11-16 22:27:56 +0400

    Tutorial 6,7

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,8 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.contrib import admin
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
@@ -14,6 +16,11 @@ class Question(models.Model):
     # def was_published_recently(self):
     #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
